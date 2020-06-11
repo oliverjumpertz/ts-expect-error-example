4c1f04c1a1208715e414cb49ef86f5b5a13eb429
Oliver Jumpertz <oliver.jumpertz@gmail.com>
2020-06-11 21:48:46 +0200

    Added explicit return type to calculateHypotenuse.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { strict as assert } from 'assert';
 
-export default function calculateHypotenuse(a: number, b: number) {
+export default function calculateHypotenuse(a: number, b: number): number {
   assert(typeof a === 'number');
   assert(typeof b === 'number');
   return Math.sqrt(a * a + b * b);
